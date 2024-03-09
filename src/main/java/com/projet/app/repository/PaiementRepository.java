754f4c1c101fa465055b58391f080f66fe4685de
Rabii khenissi <rabiikhenissi124@gmail.com>
2024-03-09 16:09:53 +0100

    a

--- src/main/java/com/projet/app/repository/PaiementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projet.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projet.app.model.Paiement;
-
-public interface PaiementRepository extends JpaRepository<Paiement, Long> {
-
-}
